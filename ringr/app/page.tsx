13db764f621f456ad2e1d22e910f51df2ac66ed0
BrandExpands <info@brandexpand.digital>
2025-12-11 16:46:25 -0500

    Delete ringr directory

--- ringr/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function HomePage() {
-  // Redirect to login page - landing page is served from /public/landing.html
-  redirect('/login')
-}
